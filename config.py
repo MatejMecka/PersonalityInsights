29ffadb61428292512ce4bcc79b51bb45fd892f4
CenTdemeern1 <timo.herngreen@gmail.com>
2023-04-01 20:51:19 +0000

    Fix typo in config file template

--- config.py
+++ config.py
@@ -9,4 +9,4 @@ trigger = '/u/PersonalityInsights'
 # Watson API
 watsonUrl = 'https://gateway.watsonplatform.net/personality-insights/api/v3/profile?version=2017-10-13'
 usernameWatson = ''
-passwordWatson
+passwordWatson = ''
